34c38cb34316dfe2eab18828e996ec6aa94a9599
Zeztron <pateltharsh@gmail.com>
2021-04-06 13:44:19 -0400

    added abstract classes for publisher and listeners

--- src/publisher.ts
+++ src/publisher.ts
@@ -1,4 +1,6 @@
 import nats from 'node-nats-streaming';
+import { TicketCreatedPublisher } from './events/TickerCreatedPublisher';
+import {} from './events/TicketCreatedEvent';
 
 console.clear();
 
@@ -10,13 +12,10 @@ const stan = nats.connect('ticketing', 'abc', {
 stan.on('connect', () => {
   console.log('Publisher connected to NATS');
 
-  const data = JSON.stringify({
+  const publisher = new TicketCreatedPublisher(stan);
+  publisher.publish({
     id: '123',
     title: 'concert',
     price: 20,
-  });
-
-  stan.publish('ticket:created', data, () => {
-    console.log('Event published');
   });
 });
